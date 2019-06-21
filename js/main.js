082876a583b6e15bcc3862ba35ef5839db704df8
Brian <brians931@gmail.com>
2019-06-21 17:07:28 +0300

    add::scrollTop-animaton for the sticky clone header to appear when scrolling is greater than 350

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    var $header = $('header');
+
+    // select header element and add a clone header before it
+    var $sticky = $header.before($header.clone().addClass('sticky'));
+
+    /**
+     * when you scroll from top past 350px or scroll up less than 350px ,
+     * add the class "scroll" to the body.
+     * with the scroll class, we change the translateY of the sticky clone header in css to 0,
+     * making it visible if distance is more than 350 or hiding it if not
+     */
+
+    $(window).on('scroll', function () {
+        var scrollFromTop = $(window).scrollTop();
+        $('body').toggleClass('scroll', (scrollFromTop > 350));
+    });
+});
