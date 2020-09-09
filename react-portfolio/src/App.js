f3c28a10784ebd8c3bd911e344564702f42968dc
Brian Sunday <brians931@gmail.com>
2020-09-09 18:28:39 +0300

    setup

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* icon twitter */}
+
+      {/* sidebar options */}
+      {/* sidebar options */}
+
+      {/* Tweet button */}
+    </div>
+  );
+}
+
+export default Sidebar;
